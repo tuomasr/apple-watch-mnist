fe6b0834c8c69bebbe212a4110897f63f8d94e34
tuomasr <tuomas.rintamaki@aalto.fi>
2017-07-23 22:18:27 +0300

    Initial Commit

---
+++ Tuomas-MNIST/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tuomas-MNIST
+//
+//  Created by Tuomas Rintamäki on 23/07/2017.
+//  Copyright © 2017 Tuomas Rintamäki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tuomas-MNISTTests/Tuomas_MNISTTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tuomas_MNISTTests.swift
+//  Tuomas-MNISTTests
+//
+//  Created by Tuomas Rintamäki on 23/07/2017.
+//  Copyright © 2017 Tuomas Rintamäki. All rights reserved.
+//
+
+import XCTest
+@testable import Tuomas_MNIST
+
+class Tuomas_MNISTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
